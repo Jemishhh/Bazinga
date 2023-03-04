57f65e914d0d81bd558e54423a20c554da473420
Burhanuddin <burhankader5252@gmail.com>
2023-03-04 14:24:50 +0530

    Backend setup

---
+++ Backend/server/db/schema.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const CONN_URL = ""
+const connectDB = async () => {
+    try{
+        console.log("Hello")
+        // mongodb connection string
+        const con = await mongoose.connect((CONN_URL), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log(`MongoDB connected : ${con.connection.host}`);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = connectDB
