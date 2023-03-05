04a0dc9ae1690afd97e7268f114282245fff7487
Devraj Anchan <devrajanchan333@gmail.com>
2023-03-05 08:00:59 +0530

    upload button

--- front-end/src/Profile/Profile.jsx
+++ front-end/src/Profile/Profile.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from "react"
+import FileUploadComponent from "./upload"
+
 
 const Profile = () => {
   return (
@@ -7,6 +9,8 @@ const Profile = () => {
     <div className="row">
         <div className="col-md-3 border-right">
             <div className="d-flex flex-column align-items-center text-center p-3 py-5"><img className="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"/><span className="font-weight-bold">Edogaru</span><span className="text-black-50">edogaru@mail.com.my</span><span> </span></div>
+            
+        <FileUploadComponent/> 
         </div>
         <div className="col-md-5 border-right">
             <div className="p-3 py-5">
