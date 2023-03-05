8e60b54101e8a3e78e9af6a2e12dbddd6ec7f14d
DarkSoul14789 <shubhrajivshah@gmail.com>
2023-03-05 11:05:53 +0530

    home page details

--- front-end/src/Profile/Profile.jsx
+++ front-end/src/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import React from "react"
+// import React from "react"
 import { upload } from "./upload"
 
 
